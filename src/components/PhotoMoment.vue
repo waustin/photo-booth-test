a0564515bddced2bc5ef10ab582173cd224e5284
Wade Austin <wade@few.io>
2020-12-09 11:08:00 -0600

    init commit

---
+++ src/components/PhotoMoment.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="photo-moment">
+        <h1>This is the photo moment</h1>
+        <div class="columns">
+            <div class="column">
+                <div class="photo-bg">
+                </div>
+            </div>
+            <div class="column is-two-fifths">
+                <p>Sidebar</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PhotoMoment",
+}
+</script>
+
+<style lang="scss" scoped>
+  .photo-bg {
+      width: 100%;
+      height: 400px;
+      display: block;
+      border: 2px solid #666;
+      background-color: #CCC;
+  }
+</style>
