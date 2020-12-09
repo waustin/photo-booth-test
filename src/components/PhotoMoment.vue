467cf76bc70a0c99cb2816566a1085bb1516e9c8
Wade Austin <wade@few.io>
2020-12-09 14:48:14 -0600

    work on testin photo moment

--- src/components/PhotoMoment.vue
+++ src/components/PhotoMoment.vue
@@ -20,7 +20,9 @@
                          v-for="(prop, idx) in props_on_stage"
                          :key="'stage-prop-' + idx" 
                          :src="prop.src" 
-                         :style="{ left: prop.x + '%', top: prop.y + '%' }"/>
+                         />
+
+                <!-- :style="{ left: prop.x + '%', top: prop.y + '%' }"/> -->
                 </div>
             </div>
             <div class="column is-two-fifths sidebar">
@@ -28,6 +30,7 @@
                 <div class="prop-list">
                     <img v-for="(prop, idx) in prop_images" 
                          :key="'prop-' + idx" :src="prop" 
+                         @dragstart="startPropDrag($event, prop)"
                          draggable="true"/>
                 </div>
             </div>
@@ -48,12 +51,26 @@ export default {
                 './images/prop-1.png', './images/prop-2.png', 
                 './images/prop-3.png', './images/prop-4.png'
             ], // the images that are dragged on top of the stage
+            prop_being_dragged: null,
             props_on_stage: [
                 // should be an array of dictionaries. { image_src/id, xpos, ypos, zindex?}
             ]
         }
     },
     methods: {
+        startPropDrag(event, prop) {
+            event.dataTransfer.dropEffect = 'move';
+            event.dataTransfer.effectAllowed = 'move';
+            event.dataTransfer.setData("propSrc", "FART");
+
+            this.prop_being_dragged = prop;
+
+            console.log('startPropDrag');
+            console.log(event.dataTransfer);
+            console.log(event.dataTransfer.types);
+            console.log(event.dataTransfer.items[0]);
+            console.log('--------');
+        },
         dragOver(event) {
           //  console.log('dragOver');
           this.is_dragging = true;
@@ -65,7 +82,7 @@ export default {
         drop(event){
             console.log('drop');
             this.is_dragging = false;
-
+/*
             // For when a file background is dropped
             let files = event.dataTransfer.files;
             if(files.length == 1 ) {
@@ -86,16 +103,18 @@ export default {
                     this.invalid_image_format = true;
                 }
             }
-            
+            */
             // Check for dropping of props
             console.log(event);
 
             let stageProp = {};
             stageProp.x = event.clientX;
             stageProp.y = event.clientY;
-
-            let imgData = event.dataTransfer.getData("text");
-            stageProp.src = imgData.src;
+/*
+            let propSrc = event.dataTransfer.getData("propSrc");
+            stageProp.src = propSrc;
+            */
+            stageProp.src = this.prop_being_dragged;
 
             this.props_on_stage.push(stageProp);
             
