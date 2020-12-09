1ddad782d47dd0019c7a703e11357eb2c116d4a7
Wade Austin <wade@few.io>
2020-12-09 11:59:47 -0600

    work on dropping an image

--- src/components/PhotoMoment.vue
+++ src/components/PhotoMoment.vue
@@ -8,7 +8,12 @@
                     @dragleave.prevent="dragLeave"
                     @drop.prevent="drop($event)">
 
-                    <img class="bg-image" v-if="user_photo"/>
+                    <img class="bg-image" 
+                        v-if="user_photo"
+                        :src="user_photo" />
+
+                    <h1 v-if="!user_photo && !is_dragging">Drop An Image</h1>
+                    <h1 v-if="invalid_image_format">Invalid file type. That was not an Image.</h1>
                 </div>
             </div>
             <div class="column is-two-fifths">
@@ -25,6 +30,7 @@ export default {
     data: function() {
         return {
             is_dragging: false,
+            invalid_image_format: false,
             user_photo: null,
             draggable_images: [], // the images that are dragged on top of the stage
             drag_images_on_stage: [
@@ -43,6 +49,7 @@ export default {
         },
         drop(event){
             console.log('drop');
+            this.is_dragging = false;
 
             let files = event.dataTransfer.files;
             if(files.length == 1 ) {
@@ -50,29 +57,45 @@ export default {
                 let file = files[0];
                 console.log(file.type)
                 if(file.type.startsWith('image/')) {
-
+                    this.invalid_image_format = false;
                     let reader = new FileReader();
                     reader.onload = (f) => {
-                        let src = f.target.result;
+                        this.user_photo = f.target.result;
                         console.log('File Loaded');
                     }
                     reader.readAsDataURL(file);
-                    
+
                 } else {
                     // File is not an image, show an error
+                    this.invalid_image_format = true;
                 }
             }
+            
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-  .photo-bg {
-      width: 100%;
-      height: 400px;
-      display: block;
-      border: 2px solid #666;
-      background-color: #CCC;
-  }
+    .photo-bg {
+        width: 100%;
+        height: 400px;
+        display: block;
+        border: 2px solid #666;
+        background-color: #CCC;
+        overflow: hidden;
+        padding: 4px;
+        display: flex;
+        position: relative;
+        justify-content: center;
+        align-items: center;
+        img {
+            max-width: 100%;
+            position: absolute;
+        }
+        img.bg-image {
+            top: 0;
+            left: 0;
+        }
+    }
 </style>
